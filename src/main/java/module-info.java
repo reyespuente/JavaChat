89647e7a278030168fc4587bac1644a859e0e19e
Puente Balcazar Reyes Guadalupe <23030505@itcelaya.edu.mx>
2025-05-16 20:27:41 -0600

    upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.javachat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javachat to javafx.fxml;
+    exports com.example.javachat;
+}
